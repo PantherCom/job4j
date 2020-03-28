a8d74bff180ad49e0e64a1b505f970b8a1ee301c
PantherCom <panther.mog@mail.ru>
2020-03-28 23:45:34 +0700

    1.1.5.4.4 макс из трёх (еще раз)

--- chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
+++ chapter_001/src/main/java/ru/job4j/condition/MultiMax.java
@@ -3,7 +3,7 @@ package ru.job4j.condition;
 
 public class MultiMax {
     public int max(int first, int second, int third) {
-        int comp = Math.max(first, second);
-        return Math.max(comp, third);
+        int comp = first > second ? first : second;
+        return comp > third ? comp : third;
     }
 }
