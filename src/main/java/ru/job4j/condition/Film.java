d4239acae6ad63b7f12ce8b6c985c459b42f1113
PantherCom <panther.mog@mail.ru>
2020-03-28 20:04:23 +0700

    1.1.5.4.2 Максимум из двух

---
+++ src/main/java/ru/job4j/condition/Film.java
@@ -0,0 +1,30 @@
+package ru.job4j.condition;
+
+public class Film {
+    public static String permission(int age) {
+        boolean can = age >= 18;
+        String label = can ? "Please, enjoy it." : "You can't watch it.";
+        //String label = age >= 18 ? "Please, enjoy it." : "You can't watch it.";
+        return label;
+    }
+
+    public static void main(String[] args) {
+        String msg = Film.permission(33);
+        System.out.println(msg);
+    }
+
+/*    public static String permission(int age) {
+        String label = "";
+        if (age >= 18) {
+            label = "Please, enjoy it.";
+        } else {
+            label = "You can't watch it.";
+        }
+        return label;
+    }
+
+    public static void main(String[] args) {
+        String msg = Film.permission(33);
+        System.out.println(msg);
+    }*/
+}
