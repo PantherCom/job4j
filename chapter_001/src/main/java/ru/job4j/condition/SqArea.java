35309a1b2d355290e9a5061c85d007864c8c7687
PantherCom <panther.mog@mail.ru>
2020-03-30 12:52:57 +0700

    1.1.5.4.5 Отладка программы

--- chapter_001/src/main/java/ru/job4j/condition/SqArea.java
+++ chapter_001/src/main/java/ru/job4j/condition/SqArea.java
@@ -1,16 +1,9 @@
 package ru.job4j.condition;
 
 public class SqArea {
-    public static double square(int p, int k) {
-        double kof = k +1;
-        double h = p / (2*kof);
-        double L = h * k;
-        double rsl = L * h;
-        return rsl;
-    }
-
-    public static void main(String[] args) {
-        double result1 = SqArea.square(6, 2);
-        System.out.println(" p = 4, k = 1, s = 1, real = " + result1);
+    public static int square(int p, int k) {
+        int h = p / (2 * (k + 1));
+        int w = k * h;
+        return w * h;
     }
 }
