e656a833d304d1d3a25d9ed055417538cbadb425
PantherCom <panther.mog@mail.ru>
2020-04-12 21:54:14 +0700

    1.1.6.5.3 шахматная доска

---
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -0,0 +1,35 @@
+package ru.job4j.loop;
+
+public class Board {
+    public static void paint(int width, int height) {
+        for (int row = 0; row < height; row++) {
+            if (row % 2 ==0){
+            for (int cell = 0; cell < width; cell++) {
+                if (cell % 2 == 0) { //печатаем X - width раз.
+                    System.out.print("X");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            } else {
+                for (int cell =0; cell<width; cell++){
+                    if (cell % 2 != 0) { //печатаем X - width раз.
+                        System.out.print("X");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+
+
+            }
+            // добавляем перевод на новую строку.
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        paint(3, 3);
+        System.out.println();
+        paint(10, 10);
+    }
+
+}
