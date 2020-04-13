fed5886030b731439050ce185205e4280dd5be3d
PantherCom <panther.mog@mail.ru>
2020-04-13 22:55:47 +0700

    1.1.6.5.3 шахматная доска #2

--- chapter_001/src/main/java/ru/job4j/loop/Board.java
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -2,7 +2,17 @@ package ru.job4j.loop;
 
 public class Board {
     public static void paint(int width, int height) {
+
         for (int row = 0; row < height; row++) {
+            for (int cell = 0; cell < width; cell++) {
+                int serv = row +cell;
+                if (serv % 2 == 0) { //печатаем X - width раз.
+                    System.out.print("X");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+        /*for (int row = 0; row < height; row++) {
             if (row % 2 ==0){
             for (int cell = 0; cell < width; cell++) {
                 if (cell % 2 == 0) { //печатаем X - width раз.
@@ -21,7 +31,7 @@ public class Board {
                 }
 
 
-            }
+            }*/
             // добавляем перевод на новую строку.
             System.out.println();
         }
@@ -29,7 +39,7 @@ public class Board {
     public static void main(String[] args) {
         paint(3, 3);
         System.out.println();
-        paint(10, 10);
+        paint(4, 4);
     }
 
 }
