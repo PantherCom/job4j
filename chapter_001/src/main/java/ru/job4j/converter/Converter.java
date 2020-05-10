908991a78c4dc54023904a2602411d643da408ca
PantherCom <panther.mog@mail.ru>
2020-05-10 17:51:59 +0700

    1.1.6.5.8 JavaDoc

--- chapter_001/src/main/java/ru/job4j/converter/Converter.java
+++ chapter_001/src/main/java/ru/job4j/converter/Converter.java
@@ -1,25 +1,53 @@
 package ru.job4j.converter;
 
+/**
+ * Класс для конвертирования валют
+ *
+ * @author Могилатов Артем
+ * @version 1
+ * @since 10.05.2020
+ **/
+
 public class Converter {
+
+    /**
+     * вычисляет количество сконвертированой валюты
+     *
+     * @param value
+     * @return количество сконвертированой валюты
+     */
     public static int rubleToEuro(int value) {
         int rsl = value / 70;
         return rsl;
     }
+
+    /**
+     * вычисляет количество сконвертированой валюты
+     *
+     * @param value
+     * @return количество сконвертированой валюты
+     */
 
     public static int rubleToDollar(int value) {
         int rsl = value / 60; // формула перевода рублей в доллоры.
         return rsl;
     }
 
+    /**
+     * вывод на экран количества конвертируемой и сконвертированой валюты.
+     * вывод результата теста на корректность вычесления
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         int value = 700;
         int expected = 10;
         int euro = Converter.rubleToEuro(value);
         boolean passed = expected == euro;
-        System.out.println( value + " rubles are " + euro + " euro.Test result : " + passed);
+        System.out.println(value + " rubles are " + euro + " euro.Test result : " + passed);
 
         int dollar = Converter.rubleToDollar(value);
-        System.out.println( value + " rubles are " + dollar + " dollar.");
+        System.out.println(value + " rubles are " + dollar + " dollar.");
 
     }
 }
