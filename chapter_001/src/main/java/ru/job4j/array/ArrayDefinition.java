aa41f3a65d684c917370544f84c71c3c2f88f82c
PantherCom <panther.mog@mail.ru>
2020-05-13 00:15:19 +0700

    1.1.7.6.0.2 Заполнение массива

--- chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -6,5 +6,17 @@ public class ArrayDefinition {
         String[] surname = new String[100500];
         float[] prices = new float[40];
 
+        String[] names = new String[4];
+        names[1] = "first";
+        names[2] = "second";
+        names[3] = "thrid";
+        names[0] = "fourth";
+
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+        System.out.println(names[0]);
+
+
     }
 }
