ae60517354abf61f339fb71f74090f7682fa5420
PantherCom <panther.mog@mail.ru>
2021-02-25 01:56:17 +0700

    1.1.7.6.8 дефрагментация массива

---
+++ chapter_001/src/main/java/ru/job4j/array/Defragment.java
@@ -0,0 +1,33 @@
+package ru.job4j.array;
+
+public class Defragment {
+    public static String[] compress(String[] array) {
+        for (int index = 0; index < array.length; index++) {
+            if (array[index] == null) {
+                int point = index;
+                int place = index + 1;
+                for (int j = place; j < array.length; j++) {
+                    if (array[j] != null) {
+                        String temp = array[point];
+                        array[point] = array[j];
+                        array[j] = temp;
+                        break;
+
+                    }
+                }
+
+            }
+            System.out.print(array[index] + " ");
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+        String[] compressed = compress(input);
+        System.out.println();
+        for (int index = 0; index < compressed.length; index++) {
+            System.out.print(compressed[index] + " ");
+        }
+    }
+}
