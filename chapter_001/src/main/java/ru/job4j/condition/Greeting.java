c082a18a1cb0830aeb854ba2d8f19b52a96072f1
PantherCom <panther.mog@mail.ru>
2021-08-31 00:47:12 +0700

    9.1. String [#237407]

---
+++ chapter_001/src/main/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+import java.sql.SQLOutput;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        System.out.println(idea + " " + "But I am a newbie.");
+        String but = "But I am a newbie.";
+        int year = 2021;
+        System.out.println(idea + " " + year);
+    }
+}
