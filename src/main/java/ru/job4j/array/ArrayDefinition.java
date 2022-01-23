5486f48c4b196d3aac080b183135dcc51c701e0c
PantherCom <panther.mog@mail.ru>
2022-01-23 22:30:23 +0700

    6.0.1.1. Размер массива [#333582]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -6,6 +6,10 @@ public class ArrayDefinition {
         String[] surname = new String[100500];
         float[] prices = new float[40];
 
+        System.out.println("Размер массива ages равен: " + ages.length);
+        System.out.println("Размер массива surname равен: " + surname.length);
+        System.out.println("Размер массива prices равен: " + prices.length);
+
         String[] names = new String[4];
         names[0] = "first";
         names[1] = "second";
